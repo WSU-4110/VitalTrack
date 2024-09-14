d312bf9edde6d518804e07258ea27f41c2d1da31
merwin aamir <merwinaa@amazon.com>
2024-09-14 18:26:48 -0400

    created Activity and Test folder. Created example Test File

--- VitalTrackServer/Activity/Greeting.py
+++ VitalTrackServer/Activity/Greeting.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def Greeting():
     return "<p>VitalTrack</p>"
---
+++ VitalTrackServer/Test/test_example.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+# example of a valid test (must start with test_)
+def test_example():
+    assert 1 == 1
