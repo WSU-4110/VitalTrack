69aa4c4e6b9fcc475864780506891062c712593f
MerwinAamir1 <121408458+MerwinAamir1@users.noreply.github.com>
2024-09-18 11:52:49 -0400

    Merwin aamir (#20)
    * connect to mongoDB
    
    * update flask version
    
    * add example env file
    
    * removed pycache
    
    * remove .venv and create mongo test file
    
    * change directory name from Test to test
    
    * change directory name from Test to test
    
    * change directory name from Test to test
    
    * Commit from GitHub Actions (Rename Directory)
    
    ---------
    
    Co-authored-by: merwin aamir <merwinaa@amazon.com>

---
+++ VitalTrackServer/test/remotecalls/test_mongodb_facade.py
@@ -0,0 +1,7 @@
+import pytest
+
+class TestMongoDbFacade:
+    def __init__(self):
+        self.__test_api_key = "test_key"
+    
+    # TODO: add tests once requiremnts are scoped out and facade is complete
