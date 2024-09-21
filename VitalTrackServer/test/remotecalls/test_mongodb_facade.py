30c9f8255fde58f5398709eef503441ed1934cbe
merwin aamir <merwinaa@amazon.com>
2024-09-21 17:38:22 -0400

    removed mongodb test untill scoped out

--- VitalTrackServer/test/remotecalls/test_mongodb_facade.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-class TestMongoDbFacade:
-    def __init__(self):
-        self.__test_api_key = "test_key"
-    
-    # TODO: add tests once requiremnts are scoped out and facade is complete
