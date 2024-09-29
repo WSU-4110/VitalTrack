0e7b4af7f9a24400c347919d27951e461074e81c
merwin aamir <merwinaa@amazon.com>
2024-09-29 19:15:43 -0400

    complete email and password auth, created protected routes, create settings, and create signout button

--- VitalTrackApp/App.js
+++ VitalTrackApp/App.js
@@ -1,5 +1,4 @@
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import ProtectedRoute from './src/components/ProtectedRoute';
 import {AuthProvider, useAuth} from './src/contexts/AuthContext';
