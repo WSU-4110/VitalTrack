2a5aba072f52d24c6bc9370fed8f57d989d44869
Mary Jabro <maryjabro@wayne.edu>
2024-09-29 17:49:06 -0400

    update app.py

--- VitalTrackServer/app.py
+++ VitalTrackServer/app.py
@@ -2,25 +2,18 @@ from flask import Flask
 from activity.api_greeting import greeting_blueprint
 from activity.api_get_tips import tips_blueprint
 from activity.api_create_user import create_user_blueprint
-
 from activity.api_get_user_entries import get_entries_blueprint
 from activity.api_log_entry import log_entry_blueprint
-
 from remotecalls.mongodb_facade import mongo_db_facade
 from model.user_model import User
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(greeting_blueprint)
-
-app.register_blueprint(user_blueprint)
-
 app.register_blueprint(create_user_blueprint)
 app.register_blueprint(tips_blueprint)
 app.register_blueprint(get_entries_blueprint)
 app.register_blueprint(log_entry_blueprint)
-
-CORS(app)
 
 CORS(app)
 
