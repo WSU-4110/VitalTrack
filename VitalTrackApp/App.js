88476fee3f323c96963b23f17471d0a29fe3cf6c
imadhb <64549375+imadhb@users.noreply.github.com>
2024-09-30 12:37:44 -0400

    Added the calender screen  (#59)
    * Added the calender screen with day highlights depending on mood(only a mock up), more functionality will be added
    
    * Changed CalendarScreen to Calendar
    
    * Hid the header for all the screen to have a consistent Frontend and based on team input

--- VitalTrackApp/App.js
+++ VitalTrackApp/App.js
@@ -7,6 +7,7 @@ import HomeScreen from './src/screens/Home';
 import LoginScreen from './src/screens/Login';
 import SettingsScreen from './src/screens/Settings';
 import SignupScreen from './src/screens/Signup';
+import CalendarScreen from './src/screens/Calendar';    
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
@@ -20,8 +21,8 @@ function AppNavigator() {
 
   return (
     <Stack.Navigator initialRouteName={currentUser ? 'Home' : 'Signup'}>
-      <Stack.Screen name="Signup" component={SignupScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
       <Stack.Screen name="Home">
         {() => (
           <ProtectedRoute>
@@ -29,8 +30,9 @@ function AppNavigator() {
           </ProtectedRoute>
         )}
       </Stack.Screen>
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-      <Stack.Screen name="Entries" component={EntriesScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Entries" component={EntriesScreen} options={{ headerShown: false }}/>
+       <Stack.Screen name="Calendar" component={CalendarScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 }
