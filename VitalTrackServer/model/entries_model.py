956bf205640bc6c0e51dd911305d230dc0ba1eeb
Mary Jabro <maryjabro@wayne.edu>
2024-10-22 17:18:35 -0400

    Added classify_day function to set the classification attribute

--- VitalTrackServer/model/entries_model.py
+++ VitalTrackServer/model/entries_model.py
@@ -22,4 +22,72 @@ class Entry(me.EmbeddedDocument):
             "symptoms": self.symptoms,
             "classification": self.classification
         }
+    
+    def classify_day(self): #lets classifcy a persons day based on the data they give us
+        score_map = {
+            'well_being': {
+                'Very Poor': -2,
+                'Poor': -1,
+                'Okay': 0,
+                'Good': 1,
+                'Great': 2
+            },
+            'sleep_quality': {
+                'Bad': -2,
+                'Moderate': 0,
+                'Good': 2
+            },
+            'mood': {
+                'Very Bad': -2,
+                'Bad': -1,
+                'Okay': 0,
+                'Good': 1,
+                'Great': 2
+            },
+            'stress': {
+                'High': -2,
+                'Moderate': -1,
+                'Low': 1
+            },
+            'activity': {
+                'Yoga': 1,
+                'Weights': 1,
+                'Walk': 1,
+                'Sport': 2,
+                'HIT': 2  
+            },
+            'symptoms': {
+                'Sneeze': 0,  
+                'Nausea': -1,
+                'Headache': -2,
+                'Fever': -3,
+                'Fatigue': -1  
+            },
+            'energy': {
+                'Very Low': -2,
+                'Low': -1,
+                'Moderate': 0,
+                'High': 1,
+                'Very High': 2
+            }
+        }
 
+        total_score = (
+            score_map['well_being'].get(self.well_being, 0) +
+            score_map['sleep_quality'].get(self.sleep_quality, 0) +
+            score_map['mood'].get(self.mood, 0) +
+            score_map['stress'].get(self.stress, 0) +
+            sum([score_map['activity'].get(a, 0) for a in self.activity]) +
+            sum([score_map['symptoms'].get(s, 0) for s in self.symptoms])
+        )
+
+        if total_score <= -6:
+            self.classification = "Very Bad Day"
+        elif total_score <= -3:
+            self.classification = "Bad Day"
+        elif total_score <= 0:
+            self.classification = "Okay Day"
+        elif total_score <= 4:
+            self.classification = "Good Day"
+        else:
+            self.classification = "Great Day"
